98f5470a6bfafe86f58afce855fe7a396022d30d
boying44 <boying.tang.44@gmail.com>
2016-03-23 21:56:26 -0400

    basic

---
+++ index.js
@@ -0,0 +1,12 @@
+var scale = d3.scale.linear()
+    .domain([0,d3.max()])
+    .range([0,420]);
+
+
+
+var load = function(){
+
+}
+
+window.addEventListener('load', load);
+var b = document.getElementById('');
